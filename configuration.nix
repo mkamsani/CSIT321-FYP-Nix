fa35ff4a0668308c2a07fc58d2b690ca91a3cf89
mkamsani <mkamsani@posteo.org>
2023-12-03 13:48:52 +0800

    Update config

--- configuration.nix
+++ configuration.nix
@@ -49,6 +49,23 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    cheese # webcam tool
+    gnome-music
+    gedit # text editor
+    epiphany # web browser
+    geary # email reader
+    evince # document viewer
+    gnome-characters
+    totem # video player
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+  ]);
 
   # Configure keymap in X11
   services.xserver = {
